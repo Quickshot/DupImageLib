096df729ef6ac09382003a1df24813e2f5d224ba
Jukka Teittinen <jpteitti@gmail.com>
2012-05-16 16:28:11 +0300

    Changed File and Hash to properties. Also changed Hash to be List<long>.

--- DupImage/ImageStruct.cs
+++ DupImage/ImageStruct.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace DupImage
@@ -13,34 +14,34 @@ namespace DupImage
         /// </summary>
         /// <param name="file">FileInfo to be used.</param>
         public ImageStruct(FileInfo file)
+            : this()
         {
             File = file;
 
             // Reserve space for 256 bit hash.
-            Hash = new ulong[4];
+            Hash = new List<long>(4);
         }
 
         /// <summary>
         /// Construct a new ImageStruct from image path.
         /// </summary>
         /// <param name="pathToImage">Image location</param>
-        public ImageStruct(String pathToImage)
+        public ImageStruct(String pathToImage) : this()
         {
             File = new FileInfo(pathToImage);
 
             // Reserve space for 256 bit hash.
-            Hash = new ulong[4];
+            Hash = new List<long>(4);
         }
 
         /// <summary>
         /// File information.
         /// </summary>
-        public FileInfo File;
+        public FileInfo File { get; set; }
 
         /// <summary>
-        /// Hash of the image.
+        /// Hash of the image. Uses longs instead of ulong to be CLS compliant.
         /// </summary>
-        public ulong[] Hash;
-
+        public List<long> Hash { get; set; }
     }
 }
