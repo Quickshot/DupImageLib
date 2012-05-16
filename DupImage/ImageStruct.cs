9e0f5c0a12323bd70f6d34556c3933f80110d08b
Jukka Teittinen <jpteitti@gmail.com>
2012-05-16 13:14:32 +0300

    Made initial ImageStruct structure. Idea is to store all information and hashes of image in this structure.

---
+++ DupImage/ImageStruct.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace DupImage
+{
+    /// <summary>
+    /// Structure for containing image information and hash values.
+    /// </summary>
+    struct ImageStruct
+    {
+        /// <summary>
+        /// Construct a new ImageStruct from FileInfo.
+        /// </summary>
+        /// <param name="file">FileInfo to be used.</param>
+        public ImageStruct(FileInfo file)
+        {
+            File = file;
+
+            // Reserve space for 256 bit hash.
+            Hash = new ulong[4];
+        }
+
+        /// <summary>
+        /// Construct a new ImageStruct from image path.
+        /// </summary>
+        /// <param name="pathToImage">Image location</param>
+        public ImageStruct(String pathToImage)
+        {
+            File = new FileInfo(pathToImage);
+
+            // Reserve space for 256 bit hash.
+            Hash = new ulong[4];
+        }
+
+        /// <summary>
+        /// File information.
+        /// </summary>
+        public FileInfo File;
+
+        /// <summary>
+        /// Hash of the image.
+        /// </summary>
+        public ulong[] Hash;
+
+    }
+}
