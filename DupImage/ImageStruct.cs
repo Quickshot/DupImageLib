c168d1c42d013760dd81c14ad74edf3103330546
Jukka Teittinen <jpteitti@gmail.com>
2012-05-17 16:05:58 +0300

    Changed ImageStruct to be class to avoid problems with mutable structures.

--- DupImage/ImageStruct.cs
+++ DupImage/ImageStruct.cs
@@ -7,31 +7,30 @@ namespace DupImage
     /// <summary>
     /// Structure for containing image information and hash values.
     /// </summary>
-    public struct ImageStruct
+    public class ImageStruct
     {
         /// <summary>
         /// Construct a new ImageStruct from FileInfo.
         /// </summary>
         /// <param name="file">FileInfo to be used.</param>
         public ImageStruct(FileInfo file)
-            : this()
         {
             File = file;
 
-            // Reserve space for 256 bit hash.
-            Hash = new List<long>(4);
+            // Init Hash
+            Hash = new List<long>();
         }
 
         /// <summary>
         /// Construct a new ImageStruct from image path.
         /// </summary>
         /// <param name="pathToImage">Image location</param>
-        public ImageStruct(String pathToImage) : this()
+        public ImageStruct(String pathToImage)
         {
             File = new FileInfo(pathToImage);
 
-            // Reserve space for 256 bit hash.
-            Hash = new List<long>(4);
+            // Init Hash
+            Hash = new List<long>();
         }
 
         /// <summary>
@@ -44,28 +43,5 @@ namespace DupImage
         /// </summary>
         public List<long> Hash { get; set; }
 
-        public override bool Equals(System.Object obj)
-        {
-            return obj is ImageStruct && this == (ImageStruct)obj;
-        }
-
-        public override int GetHashCode()
-        {
-            // To avoid collisions
-            var hash = 13;
-            hash = (hash * 7) + File.GetHashCode();
-            hash = (hash * 7) + Hash.GetHashCode();
-            return hash;
-        }
-
-        public static bool operator ==(ImageStruct first, ImageStruct second)
-        {
-            return first.File == second.File && first.Hash == second.Hash;
-        }
-
-        public static bool operator !=(ImageStruct first, ImageStruct second)
-        {
-            return !(first == second);
-        }
     }
 }
