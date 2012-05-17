86913909a7bc681235fc89caf6c97a6fbb50d688
Jukka Teittinen <jpteitti@gmail.com>
2012-05-17 19:57:32 +0300

    Small test case.

--- DupImageConsole/Program.cs
+++ DupImageConsole/Program.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DupImage;
 
 namespace DupImageConsole
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            // Path to image
+            var hash = ImageHashes.CalculateMedianHash64(@"D:\SkyDrive\Pictures\moominhaddockhuge.png");
+            Console.WriteLine("Hash of the image is: {0:X}", hash);
         }
     }
 }
