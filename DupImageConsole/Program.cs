2d9ad823fc27d002b695c0252e1c9bbc7d943717
Jukka Teittinen <jpteitti@gmail.com>
2012-05-19 20:05:07 +0300

    Working console program for finding similar images using median algorithm.

--- DupImageConsole/Program.cs
+++ DupImageConsole/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
+using CommandLine;
+using CommandLine.Text;
 using DupImage;
 
 namespace DupImageConsole
@@ -10,11 +13,107 @@ namespace DupImageConsole
     {
         static void Main(string[] args)
         {
-            // Path to image
-            var hash = ImageHashes.CalculateMedianHash64(@"D:\SkyDrive\Pictures\moominhaddockhuge.png");
-            Console.WriteLine("Hash of the image is: {0:X}", hash);
-            var hash2 = ImageHashes.CalculateMedianHash256(@"D:\SkyDrive\Pictures\moominhaddockhuge.png");
-            Console.WriteLine("Hash of the image is: {0:X}{1:X}{2:X}{3:X}", hash2[0], hash2[1], hash2[2], hash2[3]);
+            var options = new Options();
+            var parser = new CommandLineParser(new CommandLineParserSettings(Console.Error));
+
+            if (parser.ParseArguments(args, options))
+            {
+                // Parsed arguments, so lets get to work.
+                var dirInfo = new DirectoryInfo(options.Directory);
+                FullDirList(dirInfo, "*.*", options.Recursive);
+                // Filter files
+                var searchPattern = new Regex(@"\.(jpg|jpeg|png|tif|tiff|gif|bmp|wmp)$", RegexOptions.IgnoreCase);
+
+                var filteredFiles = files.Where(f => searchPattern.IsMatch(f.ImagePath)).ToList();
+                
+                // Lets try to calculate hashes
+                var images = new List<ImageStruct>();
+                Console.WriteLine("Calculating hashes...");
+                for (var i = 0; i < filteredFiles.Count; i++)
+                {
+                    try
+                    {
+                        if (options.LargeHash)
+                        {
+                            ImageHashes.CalculateMedianHash(filteredFiles[i], true);
+                            images.Add(filteredFiles[i]);
+                        }
+                        else
+                        {
+                            ImageHashes.CalculateMedianHash(filteredFiles[i]);
+                            images.Add(filteredFiles[i]);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        // Meh
+                    }
+                }
+                // Hash comparison
+                Console.WriteLine("Comparing hashes...");
+                for (int i = 0; i < images.Count; i++)
+                {
+                    for (int j = i+1; j < images.Count; j++)
+                    {
+                        var similarity = ImageHashes.CompareHashes(images[i], images[j]);
+                        if (similarity > 0.5f)
+                        {
+                            Console.WriteLine("Match found: Similarity {0}", similarity);
+                            Console.WriteLine("{0}", images[i].ImagePath);
+                            Console.WriteLine("{0}", images[j].ImagePath);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static List<ImageStruct> files = new List<ImageStruct>();  // List that will hold the files and subfiles in path
+        private static void FullDirList(DirectoryInfo dir, string searchPattern, bool subdirs)
+        {
+            // list the files
+            try
+            {
+                foreach (FileInfo f in dir.GetFiles(searchPattern))
+                {
+                    files.Add(new ImageStruct(f));
+                }
+            }
+            catch
+            {
+                return;  // We alredy got an error trying to access dir so dont try to access it again
+            }
+
+            // process each directory
+            // If I have been able to see the files in the directory I should also be able 
+            // to look at its directories so I dont think I should place this in a try catch block
+            if (subdirs)
+            {
+                foreach (DirectoryInfo d in dir.GetDirectories())
+                {
+                    FullDirList(d, searchPattern, true);
+                }
+            }
+        }
+
+
+        private sealed class Options : CommandLineOptionsBase
+        {
+            [Option("d", "directory", HelpText = "Directory where to load images.", Required = true)]
+            public string Directory { get; set; }
+
+            [Option("r", "recursive", HelpText = "Search subdirectories for images.")]
+            public bool Recursive { get; set; }
+
+            [Option("l", "large", HelpText = "Use 256 bit Hash.")]
+            public bool LargeHash { get; set; }
+
+            [HelpOption]
+            public string GetUsage()
+            {
+                var help = new HelpText();
+                help.AddOptions(this);
+                return help;
+            }
         }
     }
 }
