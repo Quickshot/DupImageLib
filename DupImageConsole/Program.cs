d299fb6b002bfad8e3ecbecf91daf01d10434da8
Jukka Teittinen <jpteitti@gmail.com>
2012-05-19 13:59:39 +0300

    Added some test code.

--- DupImageConsole/Program.cs
+++ DupImageConsole/Program.cs
@@ -13,6 +13,8 @@ namespace DupImageConsole
             // Path to image
             var hash = ImageHashes.CalculateMedianHash64(@"D:\SkyDrive\Pictures\moominhaddockhuge.png");
             Console.WriteLine("Hash of the image is: {0:X}", hash);
+            var hash2 = ImageHashes.CalculateMedianHash256(@"D:\SkyDrive\Pictures\moominhaddockhuge.png");
+            Console.WriteLine("Hash of the image is: {0:X}{1:X}{2:X}{3:X}", hash2[0], hash2[1], hash2[2], hash2[3]);
         }
     }
 }
