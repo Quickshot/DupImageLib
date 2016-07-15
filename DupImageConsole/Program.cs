cbcd9b46eb98ae93af91cef5316858bc1a3bfe47
Jukka Teittinen <jpteitti@gmail.com>
2016-07-15 13:54:57 +0300

    Use newest CommandLine Nuget package.

--- DupImageConsole/Program.cs
+++ DupImageConsole/Program.cs
@@ -14,104 +14,115 @@ namespace DupImageConsole
 {
     static class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            var options = new Options();
-            var parser = new CommandLineParser(new CommandLineParserSettings(Console.Error));
-
-            if (parser.ParseArguments(args, options))
+            var option = new Options();
+            var results = CommandLine.Parser.Default.ParseArguments<Options>(args);
+            var parsed = results.MapResult(
+                options =>
+                {
+                    option = options;
+                    return true;
+                },
+                errors => false);
+            if (!parsed)
             {
-                // Parsed arguments, so lets get to work.
-                Console.WriteLine("Directory being searched: \n{0}\n", options.Directory);
+                return -1;
+            }
 
-                var dirInfo = new DirectoryInfo(options.Directory);
-                FullDirList(dirInfo, "*.*", options.Recursive);
-                // Filter files
-                var searchPattern = new Regex(@"\.(jpg|jpeg|png|tif|tiff|gif|bmp|wmp)$", RegexOptions.IgnoreCase);
 
-                var filteredFiles = files.Where(f => searchPattern.IsMatch(f.ImagePath)).ToList();
-                
-                // Lets try to calculate hashes
-                // Used to lock access to List
-                var listLock = new object();
-                var exceptions = new ConcurrentQueue<Exception>();
+            // Parsed arguments, so lets get to work.
+            Console.WriteLine("Directory being searched: \n{0}\n", option.Directory);
 
-                var images = new List<ImageStruct>();
+            var dirInfo = new DirectoryInfo(option.Directory);
+            FullDirList(dirInfo, "*.*", option.Recursive);
+            // Filter files
+            var searchPattern = new Regex(@"\.(jpg|jpeg|png|tif|tiff|gif|bmp|wmp)$", RegexOptions.IgnoreCase);
 
-                // Generate dct coefficients if using dct algorithm.
-                var dctMatrix = new float[1][];
-                if (options.Algorithm == AlgorithmSelection.Dct)
+            var filteredFiles = files.Where(f => searchPattern.IsMatch(f.ImagePath)).ToList();
+
+            // Lets try to calculate hashes
+            // Used to lock access to List
+            var listLock = new object();
+            var exceptions = new ConcurrentQueue<Exception>();
+
+            var images = new List<ImageStruct>();
+
+            // Generate dct coefficients if using dct algorithm.
+            var dctMatrix = new float[1][];
+            if (option.Algorithm == AlgorithmSelection.Dct)
+            {
+                dctMatrix = ImageHashes.GenerateDctMatrix(32);
+            }
+
+            Console.WriteLine("Finding images and calculating hashes...");
+            Parallel.For(0, filteredFiles.Count, i =>
+            //for (var i = 0; i < filteredFiles.Count; i++)
+            {
+                try
                 {
-                    dctMatrix = ImageHashes.GenerateDctMatrix(32);
-                }
-
-                Console.WriteLine("Finding images and calculating hashes...");
-                Parallel.For(0, filteredFiles.Count,i =>
-                //for (var i = 0; i < filteredFiles.Count; i++)
-                {
-                    try
+                    switch (option.Algorithm)
                     {
-                        switch (options.Algorithm)
-                        {
-                            case AlgorithmSelection.Median256:
-                                ImageHashes.CalculateMedianHash(filteredFiles[i], true);
-                                lock (listLock)
-                                {
-                                    images.Add(filteredFiles[i]);
-                                }
-                                break;
-                            case AlgorithmSelection.Median:
-                                ImageHashes.CalculateMedianHash(filteredFiles[i], false);
-                                lock (listLock)
-                                {
-                                    images.Add(filteredFiles[i]);
-                                }
-                                break;
-                            default:
-                                ImageHashes.CalculateDctHash(filteredFiles[i], dctMatrix);
-                                lock (listLock)
-                                {
-                                    images.Add(filteredFiles[i]);
-                                }
-                                break;
-                        }
+                        case AlgorithmSelection.Median256:
+                            ImageHashes.CalculateMedianHash(filteredFiles[i], true);
+                            lock (listLock)
+                            {
+                                images.Add(filteredFiles[i]);
+                            }
+                            break;
+                        case AlgorithmSelection.Median:
+                            ImageHashes.CalculateMedianHash(filteredFiles[i], false);
+                            lock (listLock)
+                            {
+                                images.Add(filteredFiles[i]);
+                            }
+                            break;
+                        default:
+                            ImageHashes.CalculateDctHash(filteredFiles[i], dctMatrix);
+                            lock (listLock)
+                            {
+                                images.Add(filteredFiles[i]);
+                            }
+                            break;
                     }
-                    catch (Exception e)
-                    {
+                }
+                catch (Exception e)
+                {
                         // Shouldnt really be doing this, but if we get exception from Hash functions
                         // we know that the file being processed was not an image.
                         exceptions.Enqueue(e);
-                    }
-                });
+                }
+            });
 
-                Console.WriteLine("Found {0} pictures.\n", images.Count);
+            Console.WriteLine("Found {0} pictures.\n", images.Count);
 
-                // Hash comparison
-                Console.WriteLine("Comparing hashes...");
-                var similarCount = 0;
-                Parallel.For(0, images.Count, i =>
-                //for(var i = 0; i < images.Count; i++)
+            // Hash comparison
+            Console.WriteLine("Comparing hashes...");
+            var similarCount = 0;
+            Parallel.For(0, images.Count, i =>
+            //for(var i = 0; i < images.Count; i++)
+            {
+                for (int j = i + 1; j < images.Count; j++)
                 {
-                    for (int j = i + 1; j < images.Count; j++)
+                    var similarity = ImageHashes.CompareHashes(images[i], images[j]);
+                    if (similarity > option.Threshold)
                     {
-                        var similarity = ImageHashes.CompareHashes(images[i], images[j]);
-                        if (similarity > options.Threshold)
+                        lock (listLock)
                         {
-                            lock (listLock)
-                            {
-                                Console.WriteLine("Match found: Similarity {0}", similarity);
-                                Console.WriteLine("{0}", images[i].ImagePath);
-                                Console.WriteLine("{0}\n", images[j].ImagePath);
-                                similarCount++;
-                            }
+                            Console.WriteLine("Match found: Similarity {0}", similarity);
+                            Console.WriteLine("{0}", images[i].ImagePath);
+                            Console.WriteLine("{0}\n", images[j].ImagePath);
+                            similarCount++;
                         }
                     }
-                });
+                }
+            });
 
-                Console.WriteLine("Processed {0} images", images.Count);
-                Console.WriteLine("Rejected image count {0}", exceptions.Count);
-                Console.WriteLine("Found {0} similar image pairs.", similarCount);
-            }
+            Console.WriteLine("Processed {0} images", images.Count);
+            Console.WriteLine("Rejected image count {0}", exceptions.Count);
+            Console.WriteLine("Found {0} similar image pairs.", similarCount);
+
+            return 0;
         }
 
         private static List<ImageStruct> files = new List<ImageStruct>();  // List that will hold the files and subfiles in path
@@ -149,32 +160,24 @@ namespace DupImageConsole
             Dct
         }
 
-        private sealed class Options : CommandLineOptionsBase
+        private sealed class Options
         {
             public Options()
             {
                 Threshold = 0.9f;
             }
 
-            [Option("d", "directory", HelpText = "Directory where to load images.", Required = true)]
+            [Option('d', "directory", HelpText = "Directory where to load images.", Required = true)]
             public string Directory { get; set; }
 
-            [Option("r", "recursive", HelpText = "Search subdirectories for images.")]
+            [Option('r', "recursive", HelpText = "Search subdirectories for images.")]
             public bool Recursive { get; set; }
 
-            [Option("t", "threshold", HelpText = "Image similarity threshold value. Should be between 0 and 1, where 1 is totally similar images.")]
+            [Option('t', "threshold", HelpText = "Image similarity threshold value. Should be between 0 and 1, where 1 is totally similar images.")]
             public float Threshold { get; set; }
 
-            [Option("a", "algorithm", HelpText = "Algorithm to be used for hash calculation. Algoritms are Median|Median256|Dct.")]
+            [Option('a', "algorithm", HelpText = "Algorithm to be used for hash calculation. Algoritms are Median|Median256|Dct.")]
             public AlgorithmSelection Algorithm { get; set; }
-
-            [HelpOption]
-            public string GetUsage()
-            {
-                var help = new HelpText();
-                help.AddOptions(this);
-                return help;
-            }
         }
     }
 }
