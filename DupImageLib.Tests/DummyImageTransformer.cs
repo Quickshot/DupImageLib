9e552bbc76b4042c8628da4b2564f2bd68a79e68
Jukka Teittinen <jpteitti@gmail.com>
2017-04-19 21:18:30 +0300

    Update tests to use DummyImageTransformer when calculating hashes.

---
+++ DupImageLib.Tests/DummyImageTransformer.cs
@@ -0,0 +1,26 @@
+﻿namespace DupImageLib.Tests
+{
+    public class DummyImageTransformer : IImageTransformer
+    {
+        public byte[] TransformImage(string path, int width, int height)
+        {
+            var pixels = new byte[width * height];
+            byte pixelValue = 0;
+            for (var i = 0; i < pixels.Length; i++)
+            {
+                pixels[i] = pixelValue;
+
+                if (pixelValue == 255)
+                {
+                    pixelValue = 0;
+                }
+                else
+                {
+                    pixelValue++;
+                }
+            }
+
+            return pixels;
+        }
+    }
+}
--- DupImageLib.Tests/ImageHashesTests.cs
+++ DupImageLib.Tests/ImageHashesTests.cs
@@ -5,10 +5,17 @@ namespace DupImageLib.Tests
 {
     public class ImageHashesTests
     {
+        private readonly ImageHashes _imgHashes;
+
+        public ImageHashesTests()
+        {
+            _imgHashes = new ImageHashes(new DummyImageTransformer());
+        }
+
         [Fact]
         public void Test1()
         {
-            var hash = ImageHashes.CalculateMedianHash64(@"C:\Users\jptei\Dropbox\Pictures\Wallpapers Additional\00a10f6575cf41bce326f03bf5482fc2440727c4.jpg");
+            var hash = _imgHashes.CalculateMedianHash64(@"C:\Users\jptei\Dropbox\Pictures\Wallpapers Additional\00a10f6575cf41bce326f03bf5482fc2440727c4.jpg");
 
             Assert.NotEqual(0L, hash);
         }
