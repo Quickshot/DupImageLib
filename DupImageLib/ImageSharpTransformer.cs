17bb0b8f1723b762606cfb20197eac645da66fcd
Jukka Teittinen <jpteitti@gmail.com>
2017-11-26 12:54:46 +0200

    Added ImageSharpTransformer
    Uses ImageSharp for image manipulation operations

---
+++ DupImageLib/ImageSharpTransformer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using SixLabors.ImageSharp;
+
+namespace DupImageLib
+{
+    public class ImageSharpTransformer : IImageTransformer
+    {
+        public byte[] TransformImage(Stream stream, int width, int height)
+        {
+            byte[] imageBytes;
+            using (Image<Rgba32> image = Image.Load(stream))
+            {
+                image.Mutate(x => x
+                    .Resize(width, height)
+                    .Grayscale());
+                imageBytes = image.SavePixelData();
+            }
+            byte[] bytes = new byte[imageBytes.Length/4];
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = imageBytes[i * 4];
+            }
+
+            return bytes;
+        }
+    }
+}
